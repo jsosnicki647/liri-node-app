3680c80cfd2b246cb38691be9d41a1cdaa14d7d8
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-08-24 15:30:58 -0400

    spotifyThis

--- liri.js
+++ liri.js
@@ -3,12 +3,48 @@ require("dotenv").config()
 var moment = require("moment")
 var axios = require("axios")
 var keys = require("./keys.js")
-// var spotify = new Spotify(keys.spotify)
+var Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify)
 var arg = process.argv
 var func = arg[2]
 
+// function Spotify(id, secret){
+//     this.id = id
+//     this.secret = secret
+// }
+
 if(func == "concert-this"){
     concertThis()
+}
+else if(func == "spotify-this-song"){
+    spotifyThis()
+}
+
+function spotifyThis(){
+    var song = ""
+
+    for(n=3; n<arg.length; n++){
+        if(n == 3){
+            song = song + arg[3]
+        }
+        else{
+            song = song + " " + arg[n]
+        }
+    }
+
+
+    spotify.search({type: "track", query: song})
+    .then(function(response){
+        var artist = response.tracks.items[0].album.artists[0].name
+        var album = response.tracks.items[0].album.name
+        var previewURL = response.tracks.items[0].preview_url
+        console.log("---------------")
+        console.log(artist)
+        console.log(song)
+        console.log(previewURL)
+        console.log(album)
+        console.log("---------------")
+    })
 }
 
 function concertThis(){
@@ -43,9 +79,11 @@ function concertThis(){
             }
 
             for(k=0; k<venueArr.length; k++){
+                console.log("---------------")
                 console.log("\nDate: " + venueArr[k].date)
                 console.log("Venue: " + venueArr[k].name)
                 console.log("Location: " + venueArr[k].location)
+                console.log("---------------")
             }
         })
 }
