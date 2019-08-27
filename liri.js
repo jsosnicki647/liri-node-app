6105acf9b4cb62d700edbe54d2f65f48a07645cd
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-08-26 20:58:45 -0400

    updated functions to accept params argument

--- liri.js
+++ liri.js
@@ -4,42 +4,113 @@ var moment = require("moment")
 var axios = require("axios")
 var keys = require("./keys.js")
 var Spotify = require('node-spotify-api')
+var fs = require("fs")
 var spotify = new Spotify(keys.spotify)
 var arg = process.argv
 var func = arg[2]
+var params = ""
 
-// function Spotify(id, secret){
-//     this.id = id
-//     this.secret = secret
-// }
+for(t=3; t<arg.length; t++){
+    if(t == 3){
+        params = params + arg[3]
+    }
+    else{
+        params = params + " " + arg[t]
+    }
+}
 
 if(func == "concert-this"){
-    concertThis()
+    concertThis(params)
 }
 else if(func == "spotify-this-song"){
-    spotifyThis()
+    spotifyThis(params)
+}
+else if(func == "movie-this"){
+    movieThis(params)
+}
+else if (func == "do-what-it-says"){
+    doWhatItSays()
 }
 
-function spotifyThis(){
-    var song = ""
+function doWhatItSays(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
 
-    for(n=3; n<arg.length; n++){
-        if(n == 3){
-            song = song + arg[3]
-        }
-        else{
-            song = song + " " + arg[n]
-        }
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+        return console.log(error);
     }
+
+    // We will then print the contents of data
+    console.log(data);
+
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+    if(dataArr[0] == "concert-this"){
+        concertThis()
+    }
+    else if(dataArr[0] == "spotify-this-song"){
+        spotifyThis()
+    }
+    else if(dataArr[0] == "movie-this"){
+        movieThis()
+    }
+
+    })
+}
+
+
+function movieThis(movie){
+    if(arg.length == 3){
+        movie = "Mr. Nobody"
+    }
+
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
+    .then(
+        function(response) {
+            var title = response.data.Title
+            var year = response.data.Year
+            var imdbRating = response.data.imdbRating
+            var country = response.data.Country
+            var lang = response.data.Language
+            var plot = response.data.Plot
+            var actors = response.data.Actors
+
+            for(s=0; s<response.data.Ratings.length; s++){
+                if(response.data.Ratings[s].Source == "Rotten Tomatoes"){
+                    var rtRating = response.data.Ratings[s].Value
+                }
+                else if(s == response.data.Ratings.length){
+                    var rtRating = "N/A"
+                }
+            }
+
+            console.log("---------------")
+            console.log("Title: " + title)
+            console.log("Year: " + year)
+            console.log("IMDB Rating: " + imdbRating)
+            console.log("Rotten Tomatoes Rating: " + rtRating)
+            console.log("Country: " + country)
+            console.log("Language: " + lang)
+            console.log("Plot: " + plot)
+            console.log("Actors: " + actors)
+            console.log("---------------")
+        }
+    )
+}
+
+function spotifyThis(song){
 
     spotify.search({type: "track", query: song})
     .then(function(response){
         if(response.tracks.items.length == 0){
             console.log("---------------")
-            console.log("Ace of Base")
-            console.log("The sign")
-            console.log("https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=7373e40052d6425eac5ae5d83381a438")
-            console.log("The Sign (US Album) [Remastered]")
+            console.log("Artist: Ace of Base")
+            console.log("Song Title: The sign")
+            console.log("Preview: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=7373e40052d6425eac5ae5d83381a438")
+            console.log("Album: The Sign (US Album) [Remastered]")
             console.log("---------------")
         }
         else{
@@ -50,10 +121,10 @@ function spotifyThis(){
                     var album = response.tracks.items[p].album.name
                     var previewURL = response.tracks.items[p].preview_url
                     console.log("---------------")
-                    console.log(artist)
-                    console.log(name)
-                    console.log(previewURL)
-                    console.log(album)
+                    console.log("Artist: " + artist)
+                    console.log("Song Title: " + name)
+                    console.log("Preview: " + previewURL)
+                    console.log("Album: " + album)
                     console.log("---------------")
                 }
             }
@@ -61,12 +132,8 @@ function spotifyThis(){
     })
 }
 
-function concertThis(){
-    var artist = ""
-
-    for(i=3; i<arg.length; i++){
-        artist = artist + arg[i]
-    }
+function concertThis(artist){
+    var artist = artist.split(" ").join("")
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(
@@ -94,7 +161,7 @@ function concertThis(){
 
             for(k=0; k<venueArr.length; k++){
                 console.log("---------------")
-                console.log("\nDate: " + venueArr[k].date)
+                console.log("Date: " + venueArr[k].date)
                 console.log("Venue: " + venueArr[k].name)
                 console.log("Location: " + venueArr[k].location)
                 console.log("---------------")
